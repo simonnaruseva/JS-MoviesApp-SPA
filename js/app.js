88a48e2d7c2eea30f8c1dfd85c60b15c6ba7b2a7
Simona Ruseva <simonnaruseva68@gmail.com>
2023-03-07 13:45:45 -0700

    Loading Home Page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 window.addEventListener('load', () => {
     const app = Sammy('#container', function() {
+        this.use('Handlebars', 'hbs');
+
         this.get('/', home);
         this.get('index.html', home);
         this.get('#/home', home);
@@ -9,7 +11,12 @@ window.addEventListener('load', () => {
 
     app.run();
 
-    function home() {
-        this.swap('<h1>Sammy is working</h1>');
+    async function home() {
+        this.partials = {
+            header: await this.load('./templates/common/header.hbs'),
+            footer: await this.load('./templates/common/footer.hbs')
+        };
+
+        this.partial('./templates/home.hbs');
     }
 })
