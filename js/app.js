139e7f004b2cb25d1a5d1cd9fd0ec9acdc34b606
Simona Ruseva <simonnaruseva68@gmail.com>
2023-03-07 13:36:05 -0700

    set up - Sammy, Handelbars and output basic templates

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/* globals Sammy */
+
+window.addEventListener('load', () => {
+    const app = Sammy('#container', function() {
+        this.get('/', home);
+        this.get('index.html', home);
+        this.get('#/home', home);
+    });
+
+    app.run();
+
+    function home() {
+        this.swap('<h1>Sammy is working</h1>');
+    }
+})
